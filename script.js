bd8b2defe9a23c3956353b4433677ab80e22b8e9
Jeferson Oliveira <gavb.joliveira@unimedpoa.com.br>
2023-06-09 10:33:56 -0300

    Modificações no site

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-(function () {
-        window.onload = function () {
-            new BlipChat()
-                .withAppKey('Y2xvdmlzYnJheXRlY2g6YTY0MzI1ODctMGZiNS00ZGI4LWIyY2MtZTcxNjBhOWIwNzcw')
-                .withButton({"color":"#2CC3D5","icon":""})
-                .withoutHistory()
-                .withCustomCommonUrl('https://jeffolivera01-58352.chat.blip.ai/')
-                .build();
-                
+var openBlipChat;
+let tooltipInstances;
 
+window.onload = function () {
+		
+	const appkey = "Y2xvdmlzYnJheXRlY2g6YTY0MzI1ODctMGZiNS00ZGI4LWIyY2MtZTcxNjBhOWIwNzcw";
 
-        }
-    })();
+	var client = new BlipChat()
+	.withAppKey(appkey)
+    .withoutHistory()
+	.withEventHandler(BlipChat.LOAD_EVENT, function(){
+		var iframe = document.getElementById("blip-chat-iframe");
+		iframe.contentWindow.postMessage({ code: "ShowCloseButton", showCloseButton: true }, iframe.src);	
+	});
+    
+		
+	client.build();
+
+	openBlipChat = function() {
+		client.widget._openChat();
+	}
+	
+	M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'));
+  tooltipInstances = M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+  
+  window.addEventListener('scroll', () => {
+    for(let i = 0; i < tooltipInstances.length; ++i) {
+      tooltipInstances[i]._positionTooltip();
+    }
+  });
+
+}
