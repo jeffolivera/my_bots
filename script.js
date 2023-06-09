93dbb9dd3fcc7ba43d0d7ede599cf2e163282a94
Jeferson Oliveira <gavb.joliveira@unimedpoa.com.br>
2023-06-09 10:02:36 -0300

    Modificações no site

---
+++ script.js
@@ -0,0 +1,13 @@
+(function () {
+        window.onload = function () {
+            new BlipChat()
+                .withAppKey('Y2xvdmlzYnJheXRlY2g6YTY0MzI1ODctMGZiNS00ZGI4LWIyY2MtZTcxNjBhOWIwNzcw')
+                .withButton({"color":"#2CC3D5","icon":""})
+                .withoutHistory()
+                .withCustomCommonUrl('https://jeffolivera01-58352.chat.blip.ai/')
+                .build();
+                
+
+
+        }
+    })();
